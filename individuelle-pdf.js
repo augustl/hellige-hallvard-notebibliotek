7f86d8c3ae1aef790fb3ffffb8fe39a61530ac51
August Lilleaas <august@augustl.com>
2023-04-06 10:22:20 +0200

    Kommentarer er sikkert kjekt

--- individuelle-pdf.js
+++ individuelle-pdf.js
@@ -8,7 +8,7 @@ const musescoreBinPath = path.resolve(process.argv[2])
 const notebibliotekPath = path.resolve(process.argv[3])
 const globalOutPath = path.resolve(process.argv[4])
 
-// Doesn't do fancy recursive stuff, for now, we assume that the folder structure is relatively flat
+// Gjør ikke fancy rekursive greier. Enn så lenge kan vi leve med antagelsen om en nokså flat mappestruktur
 const dirs = fs.readdirSync(notebibliotekPath)
     .map(it => ({folder: it, fullPath:  path.resolve(notebibliotekPath, it)}))
     .filter(it => fs.lstatSync(it.fullPath).isDirectory())
@@ -19,17 +19,24 @@ for (const {folder, fullPath} of dirs) {
     const outPath = path.resolve(globalOutPath, folder)
 
     fs.mkdirSync(outPath, {recursive: true})
+    // Alle filer i out-mappa er kandidater for å ryddes opp etterpå
     const filesToCleanUp = new Set(fs.readdirSync(outPath).map(it => path.resolve(outPath, it)))
 
+    // Alle musescore-filer skal PDF-ifiseres
     const files = fs.readdirSync(fullPath).filter(it => /\.mscz$/.test(it))
     for (const file of files) {
         const filePath = path.resolve(fullPath, file)
+        // PDF-en skal hete det samme som musescore-fila
         const pdfPath = path.resolve(outPath, `${path.parse(file).name}.pdf`)
+
+        // PDF-er vi faktisk lager nå skal ikke ryddes opp etterpå
         filesToCleanUp.delete(pdfPath)
 
+        // Kjør på
         cp.spawnSync(musescoreBinPath, ["-o", pdfPath, filePath], {stdio: [process.stdin, process.stdout, process.stderr]})
     }
 
+    // Fjern alle løsgjenger-filer som eventuelt ligger og slenger i mappa
     for (const file of filesToCleanUp) {
         fs.rmSync(file)
     }
--- liturgi-pdf.js
+++ liturgi-pdf.js
@@ -8,7 +8,11 @@ const musescoreBinPath = path.resolve(process.argv[2])
 const liturgiPath = path.resolve(process.argv[3])
 const buildPath =  path.resolve(process.argv[4])
 const outPath =  path.resolve(process.argv[5])
-const files = fs.readdirSync(liturgiPath).filter(it => /^\d+/.test(it)).sort()
+
+const files = fs.readdirSync(liturgiPath)
+    // Vi skal bare ha med nummererte filer - liturgifilene er nummererte med 000, 010, osv, for å få rekkefølgen riktig i ferdig PDF
+    .filter(it => /^\d+/.test(it))
+    .sort()
 
 for (const file of files) {
     const [_, sortKey] = file.match(/^(\d+)/)
@@ -16,5 +20,6 @@ for (const file of files) {
 }
 
 
+// Vår gode venn GhostScript slurper inn alle PDF-ene og lager én nydelig PDF til oss
 const pdfs = fs.readdirSync(buildPath)
 cp.spawnSync("gs", ["-dNOPAUSE", "-dBATCH", "-sDEVICE=pdfwrite", `-sOutputFile=${outPath}`, ...(pdfs.map(it => `${buildPath}/${it}`))], {stdio: [process.stdin, process.stdout, process.stderr]})
