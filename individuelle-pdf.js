35d427aee0346242276b3816f16082266ae90c96
August Lilleaas <august@augustl.com>
2023-04-07 15:00:12 +0200

    Oppdater PDF-script til å bare lage PDF-er når det faktisk trengs

--- individuelle-pdf.js
+++ individuelle-pdf.js
@@ -8,6 +8,10 @@ const musescoreBinPath = path.resolve(process.argv[2])
 const notebibliotekPath = path.resolve(process.argv[3])
 const globalOutPath = path.resolve(process.argv[4])
 
+const writePdf = (pdfPath, filePath) => {
+    cp.spawnSync(musescoreBinPath, ["-o", pdfPath, filePath], {stdio: [process.stdin, process.stdout, process.stderr]})
+}
+
 // Gjør ikke fancy rekursive greier. Enn så lenge kan vi leve med antagelsen om en nokså flat mappestruktur
 const dirs = fs.readdirSync(notebibliotekPath)
     .map(it => ({folder: it, fullPath:  path.resolve(notebibliotekPath, it)}))
@@ -32,12 +36,18 @@ for (const {folder, fullPath} of dirs) {
         // PDF-er vi faktisk lager nå skal ikke ryddes opp etterpå
         filesToCleanUp.delete(pdfPath)
 
-        // Kjør på
-        cp.spawnSync(musescoreBinPath, ["-o", pdfPath, filePath], {stdio: [process.stdin, process.stdout, process.stderr]})
+        if (!fs.existsSync(pdfPath)) {
+            console.log(`*** PDF finnes ikke, lager (${file}) `)
+            writePdf(pdfPath, filePath)
+        } else if (fs.statSync(filePath).mtime.getTime() > fs.statSync(pdfPath).mtime.getTime()) {
+            console.log(`*** Musescore-fil har endringer, lager ny PDF (${file})`)
+            writePdf(pdfPath, filePath)
+        }
     }
 
     // Fjern alle løsgjenger-filer som eventuelt ligger og slenger i mappa
     for (const file of filesToCleanUp) {
+        console.log(`*** PDF finnes men ikke Musesecore-fil, sletter PDF (${file})`)
         fs.rmSync(file)
     }
 }
